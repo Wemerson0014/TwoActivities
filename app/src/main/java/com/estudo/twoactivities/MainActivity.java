8e277a3df889ba3996c9204de3e3eb4ef22ad5cc
Wemerson Fernandes Rangel <wemersonf.rangel@outlook.com>
2020-07-21 13:24:50 -0300

    Adiciona primeiro botão

--- app/src/main/java/com/estudo/twoactivities/MainActivity.java
+++ app/src/main/java/com/estudo/twoactivities/MainActivity.java
@@ -11,42 +11,42 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String LOG_TAG = MainActivity.class.getSimpleName();
-    public static final String EXTRA_MESSAGE = "com.estudo.twoactivities.extra.MESSAGE";
-    public static final int TEXT_REQUEST = 1;
-    private TextView mReplyHeadTextView;
-    private TextView mReplyTextView;
-    private EditText mMessageEditText;
+//    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+//    public static final String EXTRA_MESSAGE = "com.estudo.twoactivities.extra.MESSAGE";
+//    public static final int TEXT_REQUEST = 1;
+//    private TextView mReplyHeadTextView;
+//    private TextView mReplyTextView;
+//    private EditText mMessageEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mMessageEditText = findViewById(R.id.editText_main);
-        mReplyHeadTextView = findViewById(R.id.text_header_reply);
-        mReplyTextView = findViewById(R.id.text_message_reply);
+//        mMessageEditText = findViewById(R.id.editText_main);
+//        mReplyHeadTextView = findViewById(R.id.text_header_reply);
+//        mReplyTextView = findViewById(R.id.text_message_reply);
     }
 
-    public void launchSecondActivity(View view) {
-        Log.d(LOG_TAG,"Button clicked");
-        Intent intent = new Intent(this, SecondActivity.class);
-        String message = mMessageEditText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivityForResult(intent, TEXT_REQUEST);
-    }
+//    public void launchSecondActivity(View view) {
+//        Log.d(LOG_TAG,"Button clicked");
+//        Intent intent = new Intent(this, SecondActivity.class);
+//        String message = mMessageEditText.getText().toString();
+//        intent.putExtra(EXTRA_MESSAGE, message);
+//        startActivityForResult(intent, TEXT_REQUEST);
+//    }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == TEXT_REQUEST) {
-            if (resultCode == RESULT_OK) {
-                String reply = data.getStringExtra(SecondActivity.EXTRA_REPLY);
-                mReplyHeadTextView.setVisibility(View.VISIBLE);
-                mReplyTextView.setText(reply);
-                mReplyTextView.setVisibility(View.VISIBLE);
-            }
-        }
-    }
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//
+//        if (requestCode == TEXT_REQUEST) {
+//            if (resultCode == RESULT_OK) {
+//                String reply = data.getStringExtra(SecondActivity.EXTRA_REPLY);
+//                mReplyHeadTextView.setVisibility(View.VISIBLE);
+//                mReplyTextView.setText(reply);
+//                mReplyTextView.setVisibility(View.VISIBLE);
+//            }
+//        }
+//    }
 }
