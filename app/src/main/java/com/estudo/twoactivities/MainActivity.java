dc477aaf59b710a0ddac397b69b589688dc7bef2
Wemerson Fernandes Rangel <wemersonf.rangel@outlook.com>
2020-07-21 13:46:01 -0300

    Cria metodo de clique do primeiro botão mostrando um toast

--- app/src/main/java/com/estudo/twoactivities/MainActivity.java
+++ app/src/main/java/com/estudo/twoactivities/MainActivity.java
@@ -1,52 +1,29 @@
 package com.estudo.twoactivities;
 
-import android.content.Intent;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-//    private static final String LOG_TAG = MainActivity.class.getSimpleName();
-//    public static final String EXTRA_MESSAGE = "com.estudo.twoactivities.extra.MESSAGE";
-//    public static final int TEXT_REQUEST = 1;
-//    private TextView mReplyHeadTextView;
-//    private TextView mReplyTextView;
-//    private EditText mMessageEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        mMessageEditText = findViewById(R.id.editText_main);
-//        mReplyHeadTextView = findViewById(R.id.text_header_reply);
-//        mReplyTextView = findViewById(R.id.text_message_reply);
+        acesHighLetterMusic();
     }
 
-//    public void launchSecondActivity(View view) {
-//        Log.d(LOG_TAG,"Button clicked");
-//        Intent intent = new Intent(this, SecondActivity.class);
-//        String message = mMessageEditText.getText().toString();
-//        intent.putExtra(EXTRA_MESSAGE, message);
-//        startActivityForResult(intent, TEXT_REQUEST);
-//    }
+    public void acesHighLetterMusic() {
 
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//
-//        if (requestCode == TEXT_REQUEST) {
-//            if (resultCode == RESULT_OK) {
-//                String reply = data.getStringExtra(SecondActivity.EXTRA_REPLY);
-//                mReplyHeadTextView.setVisibility(View.VISIBLE);
-//                mReplyTextView.setText(reply);
-//                mReplyTextView.setVisibility(View.VISIBLE);
-//            }
-//        }
-//    }
+        Button acesHighButton = findViewById(R.id.button_aces_high);
+        acesHighButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "ACES HIGH", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
 }
